8d8c1f1525ae1df63321572e99abc14908ff3cd1
Renee Vandervelde <Renee@ReneeVandervelde.com>
2024-06-15 10:39:25 -0500

    Add font to theme

--- sample-android/src/main/kotlin/example/MainActivity.kt
+++ sample-android/src/main/kotlin/example/MainActivity.kt
@@ -1,23 +1,28 @@
 package example
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.inkapplications.ui.example.R
 import ink.ui.render.compose.ComposeRenderer
 import ink.ui.render.compose.theme.ComposeRenderTheme
-import ink.ui.structures.TextStyle
-import ink.ui.structures.elements.TextElement
-import ink.ui.structures.elements.UiElement
-import ink.ui.structures.layouts.ScrollingListLayout
-import ink.ui.structures.layouts.UiLayout
+import ink.ui.render.compose.theme.TypographyVariant
 
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val font = FontFamily(
+                Font(R.font.roboto_mono_light, FontWeight.Normal),
+                Font(R.font.roboto_mono_medium, FontWeight.Bold),
+            )
             ComposeRenderer(
-                ComposeRenderTheme()
+                ComposeRenderTheme(
+                    typography = TypographyVariant().withFontFamily(font),
+                )
             ).render(
                 SampleScreen
             )
