c910e2e43e1828420e2b76d1cf61f41e81cdd53a
Renee Vandervelde <Renee@ReneeVandervelde.com>
2024-10-19 07:54:25 -0500

    Add builders to link nav and breadcrumb elements

--- render-web-common/src/commonMain/kotlin/ink/ui/render/web/elements/LinkNavigation.kt
+++ render-web-common/src/commonMain/kotlin/ink/ui/render/web/elements/LinkNavigation.kt
@@ -6,9 +6,18 @@ data class LinkNavigation(
     val items: List<NavigationItem>,
 ): UiElement.Static {
     constructor(vararg items: NavigationItem): this(items.toList())
+    constructor(builder: Builder.() -> Unit): this(Builder().apply(builder).items)
 
     data class NavigationItem(
         val text: String,
         val url: String,
     )
+
+    data class Builder(
+        val items: MutableList<NavigationItem> = mutableListOf(),
+    ) {
+        fun link(text: String, url: String) {
+            items.add(NavigationItem(text = text, url = url))
+        }
+    }
 }
