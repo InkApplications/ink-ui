c0a1276d7aeed303e4125f69fe6343cd88960348
Renee Vandervelde <Renee@ReneeVandervelde.com>
2025-06-01 12:47:03 -0500

    Support sentiment icon colors in terminal

--- render-terminal/src/commonMain/kotlin/ink/ui/render/terminal/renderer/IconRenderer.kt
+++ render-terminal/src/commonMain/kotlin/ink/ui/render/terminal/renderer/IconRenderer.kt
@@ -1,5 +1,8 @@
 package ink.ui.render.terminal.renderer
 
+import com.github.ajalt.mordant.rendering.TextColors
+import com.github.ajalt.mordant.rendering.TextStyle
+import ink.ui.structures.Sentiment
 import ink.ui.structures.Symbol
 import ink.ui.structures.Symbol.Companion.Add
 import ink.ui.structures.Symbol.Companion.Alarm
@@ -29,31 +32,61 @@ import ink.ui.structures.Symbol.Companion.Water
 import ink.ui.structures.elements.IconElement
 
 val IconRenderer = renderer<IconElement> {
-    when (element.symbol) {
-        Add -> print("\u001B[30;42m + \u001B[0m")
-        Alarm -> print("\u001B[30;43m(!)\u001B[0m")
-        AlarmOff -> print("\u001B[90;47m(!)\u001B[0m")
-        ArrowBackward -> print("\u001B[37;44m <- \u001B[0m")
-        ArrowForward -> print("\u001B[37;44m -> \u001B[0m")
-        Bed -> print("\u001B[37;45m|Zz|\u001B[0m")
-        Caution -> print("\u001B[30;43m/!\\\u001B[0m")
-        Close -> print("\u001B[37;41m x \u001B[0m")
-        Cloud -> print("\u001B[37;46m(~~)\u001B[0m")
-        Done -> print("\u001B[30;42m OK \u001B[0m")
-        Edit -> print("\u001B[37;46m ## \u001B[0m")
-        Error -> print("\u001B[31;47m !! \u001B[0m")
-        Group -> print("\u001B[30;47m oo \u001B[0m")
-        House -> print("\u001B[30;43m/\\_\u001B[0m")
-        Lock -> print("\u001B[37;42m[=]\u001B[0m")
-        LockOpen -> print("\u001B[30;43m[ ]\u001B[0m")
-        Moon -> print("\u001B[37;44m( )\u001B[0m")
-        Movie -> print("\u001B[37;40m>>|\u001B[0m")
-        Person -> print("\u001B[30;47m o+ \u001B[0m")
-        Rain -> print("\u001B[34;47m(,,)\u001B[0m")
-        Snow -> print("\u001B[37;47m*.*\u001B[0m")
-        Remove -> print("\u001B[30;41m - \u001B[0m")
-        Sunshine -> print("\u001B[33;40m(#)\u001B[0m")
-        Temperature -> print("\u001B[31;47m T~ \u001B[0m")
-        Water -> print("\u001B[36;47m ~~ \u001B[0m")
+    val glyph = when (element.symbol) {
+        Add -> " + "
+        Alarm -> "(!)"
+        AlarmOff -> "(\\)"
+        ArrowBackward -> " <- "
+        ArrowForward -> " -> "
+        Bed -> "|Zz|"
+        Caution -> "/!\\"
+        Close -> " x "
+        Cloud -> "(~~)"
+        Done -> " OK "
+        Edit -> " ## "
+        Error -> " !! "
+        Group -> " oo "
+        House -> "/\\_"
+        Lock -> "[≡]"
+        LockOpen -> "[ ]"
+        Moon -> "( )"
+        Movie -> ">>|"
+        Person -> " o+ "
+        Rain -> "(,,)"
+        Snow -> "*.*"
+        Remove -> " - "
+        Sunshine -> "(#)"
+        Temperature -> " T° "
+        Water -> " ~~ "
+        else -> "█"
     }
+
+    val style = when (element.sentiment) {
+        Sentiment.Nominal -> TextStyle(
+            color = TextColors.brightWhite.color,
+            bgColor = TextColors.gray.color
+        )
+        Sentiment.Positive -> TextStyle(
+            color = TextColors.black.color,
+            bgColor = TextColors.brightGreen.color
+        )
+        Sentiment.Negative -> TextStyle(
+            color = TextColors.black.color,
+            bgColor = TextColors.brightRed.color
+        )
+        Sentiment.Primary -> TextStyle(
+            color = TextColors.black.color,
+            bgColor = TextColors.brightMagenta.color
+        )
+        Sentiment.Caution -> TextStyle(
+            color = TextColors.black.color,
+            bgColor = TextColors.brightYellow.color
+        )
+        Sentiment.Idle -> TextStyle(
+            color = TextColors.gray.color,
+            bgColor = TextColors.white.color,
+        )
+    }
+
+    print(style(glyph))
 }
