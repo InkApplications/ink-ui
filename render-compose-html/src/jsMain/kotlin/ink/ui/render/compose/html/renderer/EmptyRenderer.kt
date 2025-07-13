52e611fdd52a7beb06cb3f7ef9561caf745a1a65
Renee Vandervelde <Renee@ReneeVandervelde.com>
2025-07-13 11:53:00 -0500

    Workaround for compiler bug preventing web publishing

--- render-compose-html/src/jsMain/kotlin/ink/ui/render/compose/html/renderer/EmptyRenderer.kt
+++ render-compose-html/src/jsMain/kotlin/ink/ui/render/compose/html/renderer/EmptyRenderer.kt
@@ -1,8 +1,20 @@
 package ink.ui.render.compose.html.renderer
 
+import androidx.compose.runtime.Composable
 import ink.ui.structures.elements.EmptyElement
+import ink.ui.structures.elements.UiElement
+import ink.ui.structures.render.RenderResult
 import org.jetbrains.compose.web.dom.Span
 
-val EmptyRenderer = renderer<EmptyElement> { element ->
-    Span {}
+object EmptyRenderer: ElementRenderer
+{
+    @Composable
+    override fun render(
+        element: UiElement,
+        parent: ElementRenderer,
+    ): RenderResult {
+        if (element != EmptyElement) return RenderResult.Skipped
+        Span {}
+        return RenderResult.Rendered
+    }
 }
