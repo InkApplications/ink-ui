9bd5165e800081ff21c530390cb4f18c3f4d4042
Renee Vandervelde <Renee@ReneeVandervelde.com>
2025-07-13 09:39:32 -0500

    Clean up install directory paths

--- cli/src/main/bash/wrapper-template.sh
+++ cli/src/main/bash/wrapper-template.sh
@@ -4,7 +4,7 @@ VERSION={{VERSION}}
 URL=https://github.com/InkApplications/ink-ui/releases/download/${VERSION}/inkui-${VERSION}.zip
 SHA256={{HASH}}
 INSTALL_DIR="$HOME/.local/share/inkui"
-INKUI_BIN="$HOME/.local/share/inkui/inkui-${VERSION}/bin/inkui"
+INKUI_BIN="$HOME/.local/share/inkui/${VERSION}/bin/inkui"
 
 if [ ! -f "$INKUI_BIN" ]; then
     echo "Downloading inkui..."
@@ -16,8 +16,9 @@ if [ ! -f "$INKUI_BIN" ]; then
         echo "Checksum verification failed!"
         exit 1
     fi
-    mkdir -p "${INSTALL_DIR}/${SHA256}"
-    unzip $ZIP_FILE -d "${INSTALL_DIR}/${SHA256}"
+    mkdir -p "${INSTALL_DIR}/${VERSION}"
+    unzip $ZIP_FILE -d "${INSTALL_DIR}/"
+    mv "${INSTALL_DIR}/inkui-${VERSION}" "${INSTALL_DIR}/${VERSION}"
 fi
 
 "$INKUI_BIN" $@
