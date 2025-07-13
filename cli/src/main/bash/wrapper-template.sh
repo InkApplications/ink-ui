798915db60331de189b5ca575face7807af414c0
Renee Vandervelde <Renee@ReneeVandervelde.com>
2025-07-13 09:29:17 -0500

    Fix hash/sha256 renaming mixup

--- cli/src/main/bash/wrapper-template.sh
+++ cli/src/main/bash/wrapper-template.sh
@@ -10,14 +10,14 @@ if [ ! -f "$INKUI_BIN" ]; then
     echo "Downloading inkui..."
     TEMP_FOLDER=$(mktemp -d)
     ZIP_FILE="${TEMP_FOLDER}/inkui.zip"
-    curl -L $URL -o $ZIP_FILE
+    curl -L "$URL" -o "$ZIP_FILE"
     echo "$SHA256 $ZIP_FILE" | sha256sum -c -
     if [ $? -ne 0 ]; then
         echo "Checksum verification failed!"
         exit 1
     fi
-    mkdir -p "${INSTALL_DIR}/${HASH}"
-    unzip $ZIP_FILE -d "${INSTALL_DIR}/${HASH}"
+    mkdir -p "${INSTALL_DIR}/${SHA256}"
+    unzip $ZIP_FILE -d "${INSTALL_DIR}/${SHA256}"
 fi
 
 "$INKUI_BIN" $@
